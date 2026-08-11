31183436b9efe766c82cdd2f5bf15094460b5737
Ana Carvalho <abcdeoliveira@gmail.com>
2026-08-11 12:00:28 +0100

    configurando um aplicativo funcional e sem erros

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projeto-5-tripleten
 projeto 5 
+
+O projeto tem como intuito a criação de um aplicativo WEB para poder poder visualizar os dados do arquivo "vehicles.csv" com a opcção de criçaõ de um histograma ou um gráfico de dispersão.
---
+++ app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly_express as px
+import streamlit as st 
+
+st.header("Carregando o aplicativo...")
+car_data = pd.read_csv("vehicles.csv") # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Criar gráfico de dispersão') # criar um botão
+if disp_button: 
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.scatter(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
