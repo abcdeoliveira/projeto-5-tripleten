026d4fc01d438b35775f9cff7efcb7b5c0418c0c
Ana Carvalho <abcdeoliveira@gmail.com>
2026-08-13 12:24:44 +0100

    realizando alteracoes pos revisao do projeto

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly_express as px
 import streamlit as st 
 
-st.header("Carregando o aplicativo...")
+st.header("Análise de Anúncios de Venda de Carros")
 car_data = pd.read_csv("vehicles.csv") # lendo os dados
 hist_button = st.button('Criar histograma') # criar um botão
 
@@ -14,5 +14,5 @@ if hist_button:
 disp_button = st.button('Criar gráfico de dispersão') # criar um botão
 if disp_button: 
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.scatter(car_data, x="odometer")
+    fig = px.scatter(car_data, x="odometer", y='price')
     st.plotly_chart(fig, use_container_width=True)
